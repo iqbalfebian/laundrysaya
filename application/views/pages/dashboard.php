1df40cf9a5584e4032e3b1503e035110899d54ae
iqbalfebian <iqbalfebian.krw@gmail.com>
2021-10-08 16:28:37 +0700

    perbaikan fungsi logout

--- application/views/pages/dashboard.php
+++ application/views/pages/dashboard.php
@@ -220,26 +220,7 @@
   </div>
   <!--Row-->
 
-  <!-- Modal Logout -->
-  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabelLogout" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabelLogout">Konfirmasi</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          <p>Apakah anda benar ingin logout ?</p>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-outline-primary" data-dismiss="modal">Cancel</button>
-          <a href="<?= base_url() . 'index.php/logout' ?>" class="btn btn-primary">Logout</a>
-        </div>
-      </div>
-    </div>
-  </div>
+
 </div>
 </div>
 
